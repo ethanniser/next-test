7170bae8153bacab041bd92bf8eded0e245e7d2d
Ethan Niser <thebestgamer2502@gmail.com>
2023-06-07 01:46:29 -0500

    unused routes

--- src/app/_oldIndex/routeType.ts
+++
@@ -1,9 +0,0 @@
-import { DynamicRoute } from "next-typesafe-url";
-import { z } from "zod";
-
-export const Route = {
-  searchParams: z.object({
-    id: z.string(),
-  }),
-} satisfies DynamicRoute;
-export type RouteType = typeof Route;
